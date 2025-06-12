ad0b207b041e630786fbab937509164569a9f3ca
Luis Felipe Morales <145800266+LMORALESDEV@users.noreply.github.com>
2025-06-11 23:37:04 -0400

    Ejercicio de la calculadora

---
+++ 1. Actividad PrácticaTarea/1. Actividad PrácticaTarea/Controllers/CalculadoraController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _1._Actividad_PrácticaTarea.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Sumar(int NUM1, int NUM2)
+        {
+            return View(NUM1 + NUM2);
+        }
+    }
+}
