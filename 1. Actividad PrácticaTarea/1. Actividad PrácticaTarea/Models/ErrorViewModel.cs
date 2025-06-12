fe4715dd117543b460262f967e64e97a3e4f7464
Luis Felipe Morales <145800266+LMORALESDEV@users.noreply.github.com>
2025-06-11 23:23:11 -0400

    Creacion del primer ejercicio de la actividad no.1

---
+++ 1. Actividad PrácticaTarea/1. Actividad PrácticaTarea/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _1._Actividad_PrácticaTarea.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
